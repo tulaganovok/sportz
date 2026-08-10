4dad8e3c2646fb622255c3436e80b23f8e1a55a6
Otabek <tulaganovok04@gmail.com>
2026-08-10 13:08:40 +0500

    Adding server build dist folder

---
+++ server/dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running' });
+});
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
